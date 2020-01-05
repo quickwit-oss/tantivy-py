72f0e2f2177757bc1a5c5d9ae7e16fd43cc222c0
Damir Jelić <poljar@termina.org.uk>
2020-01-05 21:43:20 +0100

    ci: Use the correct docker image and set the PATH correctly.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,9 +2,10 @@
 
 docker run \
        --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
+       --env PATH=/opt/python/cp37-cp37m/bin/:/opt/python/cp38-cp38/bin/:/root/.cargo/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
        --rm \
        -v "$(pwd)":/io \
-       konstin2/maturin \
+       konstin2/maturin:master \
        publish \
        --interpreter python3.7 python3.8 \
        --username __token__ \
