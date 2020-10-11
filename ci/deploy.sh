a3a083bb887df65c6868c767599967345c52f6f1
Damir Jelić <poljar@termina.org.uk>
2020-10-11 14:21:45 +0200

    ci: Don't use the master branch of the maturin docker image.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,11 +2,9 @@
 
 docker run \
        --env MATURIN_PASSWORD="$MATURIN_PASSWORD" \
-       --env PATH=/opt/python/cp37-cp37m/bin/:/opt/python/cp38-cp38/bin/:/root/.cargo/bin:/opt/rh/devtoolset-2/root/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
        --rm \
        -v "$(pwd)":/io \
-       konstin2/maturin:master \
+       konstin2/maturin \
        publish \
-       --interpreter python3.7 python3.8 \
        --username __token__ \
        --password "$MATURIN_PASSWORD"
