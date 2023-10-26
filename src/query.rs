10e04749b3a3ba28ba79a670b4069da5b43a03f7
Jerome Berthier <jberthier@meilleursagents.com>
2023-10-26 19:48:55 +0200

    Add Query terms

--- src/query.rs
+++ src/query.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 use tantivy as tv;
+use crate::to_pyerr;
+use tv::schema::{Field, Term};
 
 /// Tantivy's Query
 #[pyclass(frozen)]
@@ -15,7 +17,79 @@ impl Query {
 
 #[pymethods]
 impl Query {
+    #[staticmethod]
+    fn term(field_id: u32, text: &str) -> Query {
+        let term = Term::from_field_text(Field::from_field_id(field_id), text);
+        Query {
+            inner: Box::new(tv::query::TermQuery::new(
+                term,
+                tv::schema::IndexRecordOption::Basic,
+            )),
+        }
+    }
+
+    #[staticmethod]
+    fn fuzzy_term(field_id: u32, distance: u8, text: &str) -> Query {
+        let ftq = tv::query::FuzzyTermQuery::new(
+            Term::from_field_text(Field::from_field_id(field_id), text),
+            distance,
+            true,
+        );
+        Query {
+            inner: (Box::new(ftq)),
+        }
+    }
+
+    #[staticmethod]
+    fn regex(field_id: u32, pattern: &str) -> PyResult<Query> {
+        let rq = tv::query::RegexQuery::from_pattern(
+            pattern,
+            Field::from_field_id(field_id),
+        )
+        .map_err(to_pyerr)?;
+        Ok(Query {
+            inner: Box::new(rq),
+        })
+    }
+
+    #[staticmethod]
+    fn phrase(field_id: u32, words: Vec<&str>) -> Query {
+        let terms = words
+            .iter()
+            .map(|&w| Term::from_field_text(Field::from_field_id(field_id), w))
+            .collect::<Vec<_>>();
+        Query {
+            inner: Box::new(tv::query::PhraseQuery::new(terms)),
+        }
+    }
+
+    #[staticmethod]
+    fn boost(q: &Query, boost: f32) -> Query {
+        let bq = tv::query::BoostQuery::new(q.get().box_clone(), boost);
+        Query {
+            inner: Box::new(bq),
+        }
+    }
+
+    #[staticmethod]
+    fn and_q(qs : Vec<PyRef<Query>>) -> Query {
+        Query {
+            inner: Box::new(tv::query::BooleanQuery::intersection(
+                qs.iter().map(|q| q.get().box_clone()).collect::<Vec<_>>()
+            ))
+        }
+    }
+
+    #[staticmethod]
+    fn or_q(qs : Vec<PyRef<Query>>) -> Query {
+        Query {
+            inner: Box::new(tv::query::BooleanQuery::union(
+                qs.iter().map(|q| q.get().box_clone()).collect::<Vec<_>>()
+            ))
+        }
+    }
+
     fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("Query({:?})", self.get()))
+        Ok(format!("{:#?}", self.get()))
     }
 }
