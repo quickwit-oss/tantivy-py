7d0dd20196d4ac808951bce2d2fa55be303a993e
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2023-12-17 19:57:25 +0900

    expose tantivy's TermQuery

--- src/query.rs
+++ src/query.rs
@@ -1,4 +1,5 @@
-use pyo3::prelude::*;
+use crate::{make_term, Schema};
+use pyo3::{exceptions, prelude::*, types::PyAny};
 use tantivy as tv;
 
 /// Tantivy's Query
@@ -18,4 +19,37 @@ impl Query {
     fn __repr__(&self) -> PyResult<String> {
         Ok(format!("Query({:?})", self.get()))
     }
+
+    /// Construct a Tantivy's TermQuery
+    #[staticmethod]
+    #[pyo3(signature = (schema, field_name, field_value, index_option = "position"))]
+    pub(crate) fn new_term_query(
+        schema: &Schema,
+        field_name: &str,
+        field_value: &PyAny,
+        index_option: &str,
+    ) -> PyResult<Query> {
+        make_term_query(schema, field_name, field_value, index_option)
+    }
+}
+
+fn make_term_query(
+    schema: &Schema,
+    field_name: &str,
+    field_value: &PyAny,
+    index_option: &str,
+) -> PyResult<Query> {
+    let term = make_term(&schema.inner, field_name, field_value)?;
+    let index_option = match index_option {
+        "position" => tv::schema::IndexRecordOption::WithFreqsAndPositions,
+        "freq" => tv::schema::IndexRecordOption::WithFreqs,
+        "basic" => tv::schema::IndexRecordOption::Basic,
+        _ => return Err(exceptions::PyValueError::new_err(
+            "Invalid index option, valid choices are: 'basic', 'freq' and 'position'"
+        ))
+    };
+    let inner = tv::query::TermQuery::new(term, index_option);
+    Ok(Query {
+        inner: Box::new(inner),
+    })
 }
--- tantivy/tantivy.pyi
+++ tantivy/tantivy.pyi
@@ -189,7 +189,9 @@ class Document:
 
 
 class Query:
-    pass
+    @staticmethod
+    def new_term_query(schema: Schema, field_name: str, field_value: Any, index_option: str = "position") -> Query:
+        pass
 
 
 class Order(Enum):
