d9fc5442653f226d175c0c5ca88a05a8b962be30
Caleb Hattingh <caleb.hattingh@gmail.com>
2024-01-22 00:36:53 +0100

    WIP

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,16 @@ def test(session):
     session.install("-rrequirements-dev.txt")
     session.install("-e", ".", "--no-build-isolation")
     session.run("pytest", *session.posargs)
+
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+def test_lindera(session):
+    session.install("-rrequirements-dev.txt")
+    session.install(
+        "--no-build-isolation",
+        '--config-settings',
+        'build-args="--features=lindera"',
+        "-e",
+        ".",
+    )
+    session.run("pytest", *session.posargs)
