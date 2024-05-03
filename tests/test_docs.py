f0cfe7445bf1360db85671dfe935f5a40c7c4cea
Caleb Hattingh <caleb.hattingh@gmail.com>
2024-05-03 23:52:38 +0200

    chore: remove useless test (#263)

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -3,9 +3,6 @@ import pytest
 
 from mktestdocs import check_md_file
 
-def test_hello():
-    assert True
-
 
 @pytest.mark.parametrize("filepath", Path("docs").glob("**/*.md"), ids=str)
 def test_docs(filepath):
